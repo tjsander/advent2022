7f89b69ec334386df91e57cacf97000cce8a87b9
Travis Sanders <tjsander@gmail.com>
2022-12-05 12:39:45 -0800

    Returning largest value in array, need elf number

--- day1/day1.py
+++ day1/day1.py
@@ -6,10 +6,17 @@ def main():
     input_file = open(INPUT, 'r')
     Lines = input_file.readlines()
     count = 0
+    elves = []
+    elves.append(0)
     for line in Lines:
         if (line == "\n"):
-            print ("ELF!" + str(count))
+            # print ("ELF!" + str(count))
+            elves.append(0)
             count += 1
+        else:
+            elves[count] += int(line)
+    print (max(elves))
+
 
 if __name__ == '__main__':
     main()
