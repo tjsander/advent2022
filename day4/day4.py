c3cdcc4924e2eb4f06fe9de6fb48c6999d2b7a2d
Travis Sanders <tjsander@gmail.com>
2022-12-05 19:43:14 -0800

    Day4: brute forced

---
+++ day4/day4.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+INPUT = 'day4/test_input.txt'
+# INPUT = 'day4/example.txt'
+
+def main():
+
+    input_file = open(INPUT, 'r')
+    Lines = input_file.readlines()
+    score = 0
+    for line in Lines:
+        out = line.strip().split(',')
+        ass1 = out[0].split('-')
+        ass2 = out[1].split('-')
+        if (int(ass1[0]) >= int(ass2[0]) and int(ass1[1]) <= int(ass2[1])):
+            score+=1
+        elif (int(ass1[0]) <= int(ass2[0]) and int(ass1[1]) >= int(ass2[1])):
+            score+=1
+
+    print (score)
+
+
+if __name__ == '__main__':
+    main()
