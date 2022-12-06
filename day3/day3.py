d19fed83047f66c57986f8d317480f82ae52e946
Travis Sanders <tjsander@gmail.com>
2022-12-05 16:39:16 -0800

    Day 3 pt 2: Thought that we were supposed to remove the badges, then do pt1 again. Wasted a bit of time writing to an intermediate file before I realized my mistake.

--- day3/day3.py
+++ day3/day3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 INPUT = 'day3/test_input.txt'
+# INPUT = 'day3/example.txt'
 
 def main():
 
@@ -8,23 +9,39 @@ def main():
     Lines = input_file.readlines()
     score = 0
 
-    for line in Lines:
-        # print (line)
-        size = int((len(line) -1) /2)
-        sack1 = set(line[0:size])
-        sack2 = set(line[size:size*2])
+    for i in range(0, int(len(Lines)/3)):
+        index = i*3
 
-        common = sack1.intersection(sack2)
-        c_letter = common.pop()
-        print (c_letter)
-        print (ord(c_letter))
+        group1 = set(Lines[index].strip())
+        group2 = set(Lines[index+1].strip())
+        group3 = set(Lines[index+2].strip())
 
-        if c_letter.isupper():
-            score += (ord(c_letter) - 38)
-        elif c_letter.islower():
-            score += (ord(c_letter) - 96)
+        common = group1.intersection(group2).intersection(group3).pop()
+
+        if common.isupper():
+            score += (ord(common) - 38)
+        elif common.islower():
+            score += (ord(common) - 96)
         else:
             print("ERROR!")
+
+    # for line in Lines:
+    #     print (line)
+    #     size = int((len(line) -1) /2)
+    #     sack1 = set(line[0:size])
+    #     sack2 = set(line[size:size*2])
+
+    #     common = sack1.intersection(sack2)
+    #     c_letter = common.pop()
+    #     print (c_letter)
+    #     print (ord(c_letter))
+
+    #     if c_letter.isupper():
+    #         score += (ord(c_letter) - 38)
+    #     elif c_letter.islower():
+    #         score += (ord(c_letter) - 96)
+    #     else:
+    #         print("ERROR!")
     print (score)
 
 if __name__ == '__main__':
